b69f731b710d473d5e2d4b3800ab731e609eea39
Chaser2010 <crawchase71@gmail.com>
2025-05-21 14:12:25 -0700

    update to switch OS

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
         element={
           <div className='homepage'>
             <div className="title">
-            <h1 class="large-font">Welcome to Chase's Website</h1>
+            <h1 className="large-font">Welcome to Chase's Website</h1>
             <h3 >This is a collection of my projects and interests.</h3>
             <h2>Projects:</h2>
             </div>
@@ -22,9 +22,9 @@ createRoot(document.getElementById('root')).render(
               <ProjectBox /> 
             </div>
             <h2 className='title'>Cool things:</h2>
-            <div className="interest1" style={{borderTop:"white solid 1px"}} ><a target="_blank"href="https://github.com/GGX404"><h3>Github</h3><p>My personal Github</p></a></div>
-            <div className="interest" ><a target='_blank'href="https://github.com/GGX404/GGX404-Website" ><h3>Source Code</h3><p>Source code for this website.</p></a></div>
-            <footer>Chase, 2025</footer>
+            <div className="interest" style={{borderTop:"white solid 1px"}} ><a target="_blank"href="https://github.com/GGX404"><h3>Github</h3><p>My personal Github</p></a></div>
+            <div className="interest" ><a target='_blank'href="https://github.com/GGX404/GGX404-Website" ><h3 style={{color: 'lightgreen'}}>Source Code</h3><p>Source code for this website.</p></a></div>
+            <footer className='homepage'>Chase, 2025</footer>
           </div>
         }
       />
